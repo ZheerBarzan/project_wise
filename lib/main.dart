ce4b97883206c17a0420ff1ab9ee68d2292896dc
ZheerBarzan <zheerbarzan85@outlook.com>
2024-02-15 18:59:11 +0300

    adding the login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_wise/home_page.dart';
+import 'package:project_wise/auth/auth.dart';
+import 'package:project_wise/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
